514c71554548d8c6b3f813b45205f56ae70f80cd
Justin Hong <jjhong922@berkeley.edu>
2022-10-25 10:54:03 -0400

    fix tests

--- tests/test_components.py
+++ tests/test_components.py
@@ -15,9 +15,9 @@ def test_dense():
 def test_resnetfc():
     key = jax.random.PRNGKey(0)
     x = jnp.ones((20, 10))
-    resnetfc = ResnetFC(10, 30)
+    resnetfc = ResnetFC(10, 30, training=True)
     params = resnetfc.init(key, x)
-    resnetfc.apply(params, x)
+    resnetfc.apply(params, x, mutable=["batch_stats"])
 
 
 def test_normalnn():
@@ -25,15 +25,15 @@ def test_normalnn():
     key, subkey = jax.random.split(key)
     x = jnp.ones((20, 10))
     cats = jax.random.choice(subkey, 3, (20, 1))
-    normalnn = NormalNN(10, 30, 3)
+    normalnn = NormalNN(10, 30, 3, training=True)
     params = normalnn.init(key, x, cats)
-    normalnn.apply(params, x, cats)
+    normalnn.apply(params, x, cats, mutable=["batch_stats"])
 
 
 def test_conditionalbatchnorm1d():
     key = jax.random.PRNGKey(0)
     x = jnp.ones((20, 10))
     y = jnp.ones((20, 1))
-    conditionalbatchnorm1d = ConditionalBatchNorm1d(10, 3)
+    conditionalbatchnorm1d = ConditionalBatchNorm1d(10, 3, training=True)
     params = conditionalbatchnorm1d.init(key, x, y)
-    conditionalbatchnorm1d.apply(params, x, y)
+    conditionalbatchnorm1d.apply(params, x, y, mutable=["batch_stats"])

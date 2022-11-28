0c632ef8d12c423ffe1cad67ff329bb7c54e3fd9
Pierre Boyeau <pierre.boyeau@gmail.com>
2022-11-28 08:28:10 -0800

    distance based stat

--- src/scvi_v2/_utils.py
+++ src/scvi_v2/_utils.py
@@ -2,7 +2,10 @@ import jax
 import jax.numpy as jnp
 
 
-def geary_c(w, x):
+def geary_c(
+    w: jnp.ndarray,
+    x: jnp.ndarray,
+):
     """Geary's C statistic.
 
     x: array of shape (n,)
@@ -16,6 +19,30 @@ def geary_c(w, x):
     return num.sum() / denom
 
 
+def nn_statistic(
+    w: jnp.ndarray,
+    x: jnp.ndarray,
+):
+    groups_mat = x[:, None] - x[None, :]
+    groups_mat = (groups_mat == 0).astype(int)
+
+    processed_mat1 = groups_mat - jnp.diag(groups_mat.diagonal())
+    is_diff_group_or_id = processed_mat1 == 0
+    penalties1 = jnp.zeros(w.shape)
+    penalties1 += is_diff_group_or_id * jnp.inf
+    d1 = w + penalties1
+    distances_to_same = d1.min(axis=1)
+
+    is_same_group = processed_mat1 > 0
+    penalties2 = jnp.zeros(w.shape)
+    penalties2 += is_same_group * jnp.inf
+    d2 = w + penalties2
+    distances_to_diff = d2.min(axis=1)
+
+    delta = distances_to_same - distances_to_diff
+    return delta.mean()
+
+
 def permutation_test(distances, node_colors, statistic="geary", n_mc_samples=1000, alternative="less"):
     """Permutation test for guided analyses."""
 
@@ -23,7 +50,10 @@ def permutation_test(distances, node_colors, statistic="geary", n_mc_samples=100
     node_colors_ = jnp.array(node_colors)
     key = jax.random.PRNGKey(0)
 
-    stat_fn = geary_c if statistic == "geary" else None
+    if statistic == "geary":
+        stat_fn = geary_c
+    elif statistic == "nn":
+        stat_fn = nn_statistic
     assert stat_fn is not None
 
     t_obs = stat_fn(distances, node_colors)
@@ -34,6 +64,7 @@ def permutation_test(distances, node_colors, statistic="geary", n_mc_samples=100
     def permute_compute(w, x, key):
         x_ = jax.random.permutation(key, x)
         return stat_fn(w, x_)
+
     t_perm = jax.vmap(permute_compute, in_axes=(None, None, 0), out_axes=0)(distances_, node_colors_, keys)
 
     if alternative == "greater":
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -18,3 +18,22 @@ def test_geary():
         ps.append(p)
     ps = np.array(ps)
     assert ps.max() >= 0.3
+
+
+def test_nn():
+    # case with spatial correlation
+    pos = np.random.randn(100, 2)
+    w = pairwise_distances(pos)
+    x = pos[:, 0] >= 0
+    x = x.astype(int)
+    assert permutation_test(w, x, statistic="nn", alternative="greater") < 0.05
+
+    # case without expected
+    ps = []
+    for _ in range(10):
+        x = np.random.randn(100) >= 0
+        x = x.astype(int)
+        p = permutation_test(w, x, statistic="nn", alternative="greater")
+        ps.append(p)
+    ps = np.array(ps)
+    assert ps.max() >= 0.3

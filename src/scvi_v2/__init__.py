fa96505d76cc2e20b5978f9386c02e3762e499fb
Pierre Boyeau <pierre.boyeau@gmail.com>
2022-11-28 08:36:56 -0800

    add init

--- src/scvi_v2/__init__.py
+++ src/scvi_v2/__init__.py
@@ -3,6 +3,8 @@ from importlib.metadata import version
 from ._model import MrVI
 from ._module import MrVAE
 
-__all__ = ["MrVI", "MrVAE", "DecoderZX", "DecoderUZ", "EncoderXU"]
+from ._utils import permutation_test
+
+__all__ = ["MrVI", "MrVAE", "DecoderZX", "DecoderUZ", "EncoderXU", "permutation_test"]
 
 __version__ = version("scvi-v2")

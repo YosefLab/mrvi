3e7c626d939e568a21ac1c6407976a7d5efd39f6
Pierre Boyeau <pierre.boyeau@gmail.com>
2023-01-13 13:44:34 -0800

    Implementation of the statistical tests (#24)
    * geary
    
    * distance based stat
    
    * add init
    
    * jit
    
    * doc + fix
    
    * docstring
    
    * default
    
    * Apply suggestions from code review
    
    fix doc
    
    Co-authored-by: Adam Gayoso <adamgayoso@users.noreply.github.com>
    
    * fix geary + rename var
    
    * added option to compute single statistic
    
    * cell score computation fn
    
    * Apply suggestions from code review
    
    pre-commit
    
    Co-authored-by: Adam Gayoso <adamgayoso@users.noreply.github.com>
    
    * pre-commit
    
    * fixes
    
    * break out similarity fn for geary's c
    
    * non vmap option
    
    * fix jit
    
    Co-authored-by: Adam Gayoso <adamgayoso@users.noreply.github.com>
    Co-authored-by: Justin Hong <jjhong922@berkeley.edu>

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,6 +7,12 @@ from scvi_v2 import MrVI
 def test_mrvi():
     adata = synthetic_iid()
     adata.obs["sample"] = np.random.choice(15, size=adata.shape[0])
+    meta1 = np.random.randint(0, 2, size=15)
+    adata.obs["meta1"] = meta1[adata.obs["sample"].values]
+
+    meta2 = np.random.randn(15)
+    adata.obs["meta2"] = meta2[adata.obs["sample"].values]
+    MrVI.setup_anndata(adata, sample_key="sample", batch_key="batch")
     adata.obs["cont_cov"] = np.random.normal(0, 1, size=adata.shape[0])
     MrVI.setup_anndata(adata, sample_key="sample", batch_key="batch", continuous_covariate_keys=["cont_cov"])
     model = MrVI(
@@ -35,5 +41,15 @@ def test_mrvi():
     )
     assert np.allclose(local_map, local_vmap, atol=1e-6)
     assert np.allclose(local_dist_map, local_dist_vmap, atol=1e-6)
+
+    donor_keys = [
+        ("meta1", "nn"),
+        ("meta2", "geary"),
+    ]
+    pvals = model.compute_cell_scores(donor_keys=donor_keys)
+    assert pvals.shape == (adata.shape[0], 2)
+    es = model.compute_cell_scores(donor_keys=donor_keys, compute_pval=False)
+    assert es.shape == (adata.shape[0], 2)
+
     # tests __repr__
     print(model)

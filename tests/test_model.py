acc488af3468f3900ad500d766ae7d35ae3e6f4b
Pierre Boyeau <pierre.boyeau@gmail.com>
2023-02-21 15:02:21 -0800

    fixes

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,6 @@
 from tempfile import TemporaryDirectory
 
 import numpy as np
-import jax.numpy as jnp
 from scvi.data import synthetic_iid
 
 from scvi_v2 import MrVI, MrVIReduction
@@ -38,9 +37,9 @@ def test_mrvi():
         15,
     )
     local_map = model.get_local_sample_representation(use_vmap=False)
-    local_dist_map = model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=False)["cell"]
-    local_dist_map = model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=True, norm="l1")["cell"]
-    local_dist_map = model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=True, norm="linf")["cell"]
+    model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=False)["cell"]
+    model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=True, norm="l1")["cell"]
+    model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=True, norm="linf")["cell"]
     local_dist_map = model.get_local_sample_distances(use_vmap=False, use_gpu_for_distances=True, norm="l2")["cell"]
     assert local_map.shape == (adata.shape[0], 15, n_latent)
     assert local_dist_map.shape == (
@@ -60,7 +59,7 @@ def test_mrvi():
     assert np.allclose(local_normalized_dists[0].values, local_normalized_dists[0].values.T, atol=1e-6)
 
     # Test memory efficient groupby.
-    grouped_dists_no_cell = model.get_local_sample_distances(keep_cell=False, groupby=["meta1", "meta2"])
+    model.get_local_sample_distances(keep_cell=False, groupby=["meta1", "meta2"])
     grouped_dists_no_cell = model.get_local_sample_distances(
         keep_cell=False, groupby=["meta1", "meta2"], use_gpu_for_distances=False
     )
@@ -79,18 +78,6 @@ def test_mrvi():
 
     # tests __repr__
     print(model)
-
-    mask = np.random.choice(n_latent, size=adata.shape[1])
-    mask = jnp.array(np.eye(n_latent)[mask])
-    px_kwargs = dict(
-        mask=mask,
-    )
-    model = MrVI(
-        adata,
-        n_latent=n_latent,
-        px_kwargs=px_kwargs,
-    )
-    model.train(1, check_val_every_n_epoch=1, train_size=0.5)
 
 
 def test_mrvi_stratifications():
